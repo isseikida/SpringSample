06e364dd14bab070dfbd53322f19837e9454d098
isseikida <rasonbura8@gmail.com>
2019-09-30 02:58:57 +0900

    CSSが反映されない原因追及の編集

--- src/main/java/com/example/demo/login/controller/HomeController.java
+++ src/main/java/com/example/demo/login/controller/HomeController.java
@@ -16,24 +16,22 @@ import com.example.demo.login.domain.model.SignupForm;
 import com.example.demo.login.domain.model.User;
 import com.example.demo.login.domain.service.UserService;
 
-
 @Controller
 public class HomeController {
-
 
 	@Autowired
 	UserService userService;
 
 	//結婚ステータスのラジオボタン用変数
-	private Map<String,String>radioMarriage;
+	private Map<String, String> radioMarriage;
 
 	//ラジオボタンの初期化メソッド(ユーザー登録画面と同じ)
-	private Map<String,String>initRadioMarrige(){
-		Map<String,String>radio = new LinkedHashMap<>();
+	private Map<String, String> initRadioMarrige() {
+		Map<String, String> radio = new LinkedHashMap<>();
 
 		//既婚、未婚をMapに格納
-		radio.put("既婚" , "true");
-		radio.put("未婚" , "false");
+		radio.put("既婚", "true");
+		radio.put("未婚", "false");
 
 		return radio;
 	}
@@ -43,58 +41,56 @@ public class HomeController {
 	public String getUserList(Model model) {
 
 		//コンテンツ部分にユーザー一覧を表示するための文字列を登録
-		model.addAttribute("contents","login/userList::userList_contents");;
+		model.addAttribute("contents", "login/userList::userList_contents");
 
 		//ユーザー一覧の生成
-		List<User>userList=userService.selectMany();
+		List<User> userList = userService.selectMany();
 
 		//Modelにユーザーリストを登録
-		model.addAttribute("userList",userList);
+		model.addAttribute("userList", userList);
 
 		//データ件数を取得
 		int count = userService.count();
-		model.addAttribute("userListCount",count);
+		model.addAttribute("userListCount", count);
 
 		return "login/homeLayout";
 	}
-
 
 	//ポイント1 : 動的URL
 	//ポイント2 : @PathVariable
 	//ユーザー詳細画面にGET用メソッド
 	@GetMapping("/userDetail/{id:.+}")
-	public String getUserDetail(@ModelAttribute SignupForm form, Model model,@PathVariable("id")String userId) {
+	public String getUserDetail(@ModelAttribute SignupForm form, Model model, @PathVariable("id") String userId) {
 
 		//ユーザーID確認(デバック)
 		System.out.println("userId =" + userId);
 
 		//コンテンツ部分にユーザー詳細を表示するための文字列を登録
-		model.addAttribute("contents","login/userDetail::userDetail_contents");
+		model.addAttribute("contents", "login/userDetail::userDetail_contents");
 
 		//結婚ステータス用ラジオボタンの初期化
 		radioMarriage = initRadioMarrige();
 
 		//ラジオボタン用のMapをModelに登録
-		model.addAttribute("radioMarriage" , radioMarriage);
+		model.addAttribute("radioMarriage", radioMarriage);
 
 		//ユーザーIDのチェック
-		if(userId !=null && userId.length() > 0) {
+		if (userId != null && userId.length() > 0) {
 			//ユーザー情報を取得
 			User user = userService.selectOne(userId);
 
 			//Userクラスをフォームクラスに変換
-			form.setUserId(user.getUserId());          //ユーザーID
-			form.setUserName(user.getUserName());      //ユーザー名
-			form.setBirthday(user.getBirthday());      //誕生日
-			form.setAge(user.getAge());                //年齢
-			form.setMarriage(user.isMarriage());       //結婚ステータス
+			form.setUserId(user.getUserId()); //ユーザーID
+			form.setUserName(user.getUserName()); //ユーザー名
+			form.setBirthday(user.getBirthday()); //誕生日
+			form.setAge(user.getAge()); //年齢
+			form.setMarriage(user.isMarriage()); //結婚ステータス
 
 			//Modelに登録
-			model.addAttribute("signupForm" , form);
+			model.addAttribute("signupForm", form);
 		}
 		return "login/homeLayout";
 	}
-
 
 	//ログアウト用メソッド
 	@PostMapping("/logout")
@@ -103,7 +99,6 @@ public class HomeController {
 		//ログイン画面にリダイレクト
 		return "redirect:/login";
 	}
-
 
 	//ユーザー一覧のCSV出力用メソッド
 	@GetMapping("/userList/csv")
