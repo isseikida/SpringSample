97f99b554e3b569ccd0758cc6447ab66b44c6215
isseikida <rasonbura8@gmail.com>
2019-10-03 02:47:35 +0900

    ユーザー削除用処理

--- src/main/java/com/example/demo/login/controller/HomeController.java
+++ src/main/java/com/example/demo/login/controller/HomeController.java
@@ -121,6 +121,22 @@ public class HomeController {
 		return getUserList(model);
 	}
 
+	//ユーザー削除用処理
+	@PostMapping(value = "/userDetail",params="delete")
+	public String postUserDetailDelete(@ModelAttribute SignupForm form,Model model) {
+		System.out.println("削除ボタンの処理");
+		//削除実行
+		boolean result  = userService.deleteOne(form.getUserId());
+		if(result ==true) {
+			model.addAttribute("result","削除成功");
+		}else{
+			model.addAttribute("result", "削除失敗");
+		}
+
+		//ユーザー一覧画面を表示
+		return getUserList(model);
+	}
+
 	//ログアウト用メソッド
 	@PostMapping("/logout")
 	public String postLogout() {
