91d5b638ae12047024a364082565f994d6d545f6
isseikida <rasonbura8@gmail.com>
2019-10-13 23:30:42 +0900

    アドミン専用画面へのコントローラー追加

--- src/main/java/com/example/demo/login/controller/HomeController.java
+++ src/main/java/com/example/demo/login/controller/HomeController.java
@@ -119,7 +119,7 @@ public class HomeController {
 		user.setAge(form.getAge());
 		user.setMarriage(form.isMarriage());
 
-		
+
 	try {
 
 		//更新実行
@@ -186,4 +186,22 @@ public class HomeController {
 		//sample.csvを戻す
 		return new ResponseEntity<>(bytes , header , HttpStatus.OK);
 	}
+
+
+	//アドミン権限専用画面のGET用メソッド
+	@GetMapping("/admin")
+	public String getAdmin(Model model) {
+
+		//コンテンツ部分にユーザ詳細を表示するための文字列を登録
+		model.addAttribute("contents","login/admin::admin_contents");
+
+		//レイアウト用テンプレート
+	    return "login/homeLayout";
+	    
+	}
+
+
+
+
+
 }
