8b4dcda3c33065e949bd8b5c5c2044aadf2ebcde
isseikida <rasonbura8@gmail.com>
2019-10-22 09:49:15 +0900

    GETメソッドでユーザー全件取得一件取得

--- src/main/java/com/example/demo/login/domain/service/jdbc/RestServiceJdbcImpl.java
+++ src/main/java/com/example/demo/login/domain/service/jdbc/RestServiceJdbcImpl.java
@@ -31,13 +31,13 @@ public class RestServiceJdbcImpl implements RestService {
 	//１件検索用メソッド
 	@Override
 	public User selectOne(String userId) {
-		return null;
+		return dao.selectOne(userId);
 	}
 
 	//全件検索用メソッド
 	@Override
 	public List<User>selectMany(){
-		return null;
+		return dao.selectMany();
 	}
 
 	//１件更新用メソッド
