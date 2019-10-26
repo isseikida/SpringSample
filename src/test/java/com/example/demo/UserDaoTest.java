c172fa4aabdad263ce358a8bce025250f398726f
isseikida <rasonbura8@gmail.com>
2019-10-27 00:09:50 +0900

    仮

--- src/test/java/com/example/demo/UserDaoTest.java
+++ src/test/java/com/example/demo/UserDaoTest.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -32,5 +33,16 @@ public class UserDaoTest {
 
 		//カウントメソッドの結果が2件であることをテスト
 		assertEquals(dao.count(),2);
+	}
+
+
+	//カウントメソッドのテスト2
+	//ポイント : @Sql
+	@Test
+	@Sql("/testdata.sql")
+	public void countTest2() {
+
+		//カウントメソッドの結果が3件であることをテスト
+		assertEquals(dao.count(),3);
 	}
 }
